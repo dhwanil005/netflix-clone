aa8014d8cd13660227ac915e5803649541ef0e9e
dhwanil005 <dhwanil.shah00@gmail.com>
2023-05-11 03:59:54 -0700

    Attempt at displaying rows of movie based on genres, the url of images seems to have changed

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -36,12 +36,12 @@ function Banner() {
         backgroundPosition: "center center"
     }}>
         <div className="banner__contents">
-            <h1 className="banner__title">Movie Name</h1>
+            <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
             <div className="banner__buttons">
                 <button className="banner__button">Play</button>
                 <button className="banner__button">My List</button>
             </div>
-            <h1 className="banner_description"> {truncate(`This is a test description`,150)}</h1>
+            <h1 className="banner__description"> {truncate(movie?.overview,150)}</h1>
         </div>
         <div className="banner--fadeBottom"/>   
             </header>
--- netflix-clone/src/HomeScreen.js
+++ netflix-clone/src/HomeScreen.js
@@ -2,11 +2,43 @@ import React from 'react'
 import Nav from "./Nav"
 import Banner from "./Banner"
 import "./HomeScreen.css"
+import Row from "./Row"
+import requests from "./Requests"
+
 function HomeScreen() {
   return (
     <div>
         <Nav />
         <Banner />
+        <Row 
+        title ="Netflix Originals"
+        fetchUrl = {requests.fetchNetflixOriginals} 
+        isLargeRow />
+        <Row 
+        title ="Trending Now"
+        fetchUrl = {requests.fetchTrending} 
+        />
+        <Row 
+        title ="Action Movies"
+        fetchUrl = {requests.fetchActionMovies} 
+        />
+        <Row 
+        title ="Comedy Movies"
+        fetchUrl = {requests.fetchComedyMovies} 
+        />
+        <Row 
+        title ="Documentaries"
+        fetchUrl = {requests.fetchDocumentaries} 
+        />
+        <Row 
+        title ="Horror Movies"
+        fetchUrl = {requests.fetchHorrorMovies} 
+        />
+        <Row 
+        title ="Romance Movies"
+        fetchUrl = {requests.fetchRomanceMovies} 
+        />
+
     </div>
   )
 }
